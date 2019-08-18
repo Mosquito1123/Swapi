870fd17443c1939c40fa104852876fba0b510e8e
Tuyen <tuyendle92@gmail.com>
2019-08-18 21:58:44 +0700

    remove File.swift, which is not used

--- SwapiTests/SwapiTests.swift
+++ SwapiTests/SwapiTests.swift
@@ -18,11 +18,98 @@ class SwapiTests: XCTestCase {
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    
+    func testCharactersServiceRequest() {
+        let category = CategoryProtocolImplementation()
+        category.getCharacters(1..<10) { (datas: [Data?])  in
+            for data in datas {
+                if data == nil {
+                    assert(false, "fail to fetch all characters")
+                    return
+                }
+            }
+        }
+        assert(true, "success fetchinng all characters")
     }
+    
+    func testFilmsServiceRequest() {
+        let category = CategoryProtocolImplementation()
+        category.getFilms { (data: Data?) in
+            if data == nil {
+                assert(false, "fail to fetch films")
+                return
+            }
+        }
+        assert(true, "success fetching all films")
+    }
+    
+    func testPlanetsServiceRequest() {
+        let category = CategoryProtocolImplementation()
+        category.getPlanets(1..<8) { (datas: [Data?]) in
+            for data in datas {
+                if data == nil {
+                    assert(false, "fail to fetch all planets")
+                    return
+                }
+            }
+        }
+        assert(true, "success fetching all planets")
+    }
+    
+    func testSpeciesServiceRequest() {
+        let category = CategoryProtocolImplementation()
+        category.getSpecies(1..<5) { (datas: [Data?]) in
+            for data in datas {
+                if data == nil {
+                    assert(false, "fail to fetch all species")
+                    return
+                }
+            }
+        }
+        assert(true, "sucess fetching all species")
+    }
+    
+    func testStarshipsServiceRequest() {
+        let category = CategoryProtocolImplementation()
+        category.getStarships(1..<5) { (datas: [Data?]) in
+            for data in datas {
+                if data == nil {
+                    assert(false, "fail to fetch all species")
+                    return
+                }
+            }
+        }
+        assert(true, "success fetching all starships")
+    }
+    
+    
+    func testVehiclesServiceRequest() {
+        let category = CategoryProtocolImplementation()
+        category.getVehicles(1..<5) { (datas: [Data?]) in
+            for data in datas {
+                if data == nil {
+                    assert(false, "fail to fetch all species")
+                    return
+                }
+            }
+        }
+        assert(true, "success fetching all vehicles")
+    }
+    
+    func testAsyncServiceRequest() {
+        let launchScreenVC = LaunchScreenViewController()
+        let loadingIndicator = UIActivityIndicatorView(frame: .zero)
+        launchScreenVC.loadingIndicator = loadingIndicator
+        launchScreenVC.preFetchAllStarwarsEntities()
+        Service.dispatchGroup.notify(queue: .main) {
+            guard let _ = LocalCache.characters, let _ = LocalCache.films, let _ = LocalCache.planets, let _ = LocalCache.species,
+                  let _ = LocalCache.starships, let _ = LocalCache.vehicles else {
+                    assert(false, "async service request fail for all starwar entities")
+            }
+            assert(true, "async service request succeed for all starwar entities")
+        }
+    }
+    
 
     func testPerformanceExample() {
         // This is an example of a performance test case.
